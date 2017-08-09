b8c14d66384d76870b44c1cfa3b311e8967c3736
cmis <cmis@cmis-hs00.(none)>
2017-08-09 14:37:12 +0700

    added fundamentals

---
+++ Fundamentals1/Fundamentals1.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Fundamentals1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fundamentals1{
+    public static void dataTypes()
+    
+}
