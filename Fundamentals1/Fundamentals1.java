5561d03cc4a5734b94e48cf5c2a16a0241e661bc
cmis <cmis@cmis-hs01.(none)>
2017-08-11 08:24:43 +0700

    I have added the information about the int variable to the fundamentals program and have started on the work for char

--- Fundamentals1/Fundamentals1.java
+++ Fundamentals1/Fundamentals1.java
@@ -6,6 +6,23 @@
  * @version (a version number or a date)
  */
 public class Fundamentals1{
-    public static void dataTypes()
+    public static void intstuff(){
+    int theintvariable = 7;
+    String intdescriptionanduse = ("The int is a numerical datatype that ends withou ever having a dicimal.\n"
+    +"its maximum value is 2,147,483,647 and its minimum value is -2,147,483,648. these are both\n"
+    +"determined by its bit count witch is 32. You could probobly use this to reprisent anything that \n"
+    +" can be counted for example a list of items that you would never purchase a decimal of ,like apples \n"
+    +"could be used on a grocery list");
+    char thenumberf = 'f';
+    byte thebytevariable;
     
+   System.out.print("The int variable is "+theintvariable+" "+intdescriptionanduse);
 }
+    public static void charstuff(){
+        char thenumberf = 'f';
+        
+        
+        System.out.print("The char variable is "+thenumberf);
+}
+    }
+
