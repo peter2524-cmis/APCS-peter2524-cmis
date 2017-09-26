44e46ff03f87c84c4bf8a35663c545c70f4b9e58
cmis <cmis@cmis-hs01.(none)>
2017-09-26 14:00:14 +0700

    started the reverse function

--- fundamentals2/manipulating.java
+++ fundamentals2/manipulating.java
@@ -3,11 +3,12 @@ public class manipulating
 {
     public static void main(){
         int[] a = {1,2,3,4,5,6,7,8,9,0};
-        int[] b = {4,5,6,7,9,3,2,1,5,6,8,90,5,3,5};
+        int[] b = {4,5,6,7,9,3,2,1,5,6};
         int[] test = merge(a,b);
-        
+
         CreatingAndLooping.printArray(test,false);
     }
+
     public static int[] concat(int[] a, int[] b){
         int[] concat = new int[a.length+b.length];
         for(int i = 0; i<= concat.length; i++){
@@ -23,11 +24,22 @@ public class manipulating
 
     public static int[] merge(int[] a, int[] b){
         int[] merge = new int[a.length+b.length];
+        int alt = 0;
         merge[0] = a[0];
         for(int i =1;i<= merge.length+1; i++){
-            
+            if (i%2==1){                                               
+                alt +=1;
+                merge[i]= a[alt];
+            }
+            else{
+                merge[i] = b[alt];
+            }
         }
         return merge;
     }
+    public static void reverse(int[] array){
+        int[] stock = array;
+    }
+
 }
 
