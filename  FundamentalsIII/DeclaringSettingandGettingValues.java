c512f7c5ac9c875670446aca54e0e86bb68c7c34
cmis <cmis@cmis-hs01.(none)>
2017-10-05 11:55:40 +0700

    started fundamentals 3

---
+++  FundamentalsIII/DeclaringSettingandGettingValues.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class DeclaringSettingandGettingValues
+{
+
+    public static void main(){
+        int[] intdefault = new int[5];
+        int[][] intsquare = new int[5][5];
+
+        double[] doubledefault = new double[5];
+        double[][] doublesquare = new double[5][5];
+
+        System.out.println(Arrays.toString(intsquare));
+
+    }
+}
+
