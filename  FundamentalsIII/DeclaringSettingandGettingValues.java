06ed32ec47a13d9e812e02e28819ca0d14da9264
cmis <cmis@cmis-hs01.(none)>
2017-10-06 14:33:02 +0700

    started on problem 3 in fundamentals 3

---  FundamentalsIII/DeclaringSettingandGettingValues.java
+++  FundamentalsIII/DeclaringSettingandGettingValues.java
@@ -5,12 +5,35 @@ public class DeclaringSettingandGettingValues
     public static void main(){
         int[] intdefault = new int[5];
         int[][] intsquare = new int[5][5];
-
+        String[][] 8x8string = srprinter(8,8);
         double[] doubledefault = new double[5];
         double[][] doublesquare = new double[5][5];
-
-        System.out.println(Arrays.toString(intsquare));
-
     }
-}
 
+    public static String[][] srprinter(int rows, int cols){
+        String[][] newstringar = new String[rows][cols];
+
+        for(int i = 0; i<=rows; i++){
+            if(i%2==0){
+                for(int j = 0; j<=cols; j++){
+                    if(j%2==0){
+                        newstringar[i][j] = " ";
+                    }else{
+                        newstringar[i][j] = "#";
+                    }
+                }
+            }else{
+                for(int j = 0; j<=cols; j++){
+                    if(j%2==1){
+                        newstringar[i][j] = " ";
+                    }else{
+                        newstringar[i][j] = "#";
+                    }
+                }
+            }
+        }
+        return newstringar;
+    }
+    
+    public static String[][] proverload(String[][])
+}
