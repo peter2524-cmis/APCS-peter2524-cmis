60ffa168b0d1fe8b48ff03d5f728e91135efe611
Peter <peter2524@cmis.ac.th>
2018-01-26 11:53:31 +0700

    got through questions 3 and nearly 4 on the class design thing

--- class design/ClockDriver.java
+++ class design/ClockDriver.java
@@ -1,8 +1,12 @@
+import javax.swing.JOptionPane;
 public class ClockDriver{
     public static void main(String[] args){
         Clock kitchenClock = new Clock();
         System.out.println(kitchenClock);
-        Clock BedroomClock = new Clock(15,30,12);
+        int hours = Integer.parseInt(JOptionPane.showInputDialog("what hour is it"));
+        int mins = Integer.parseInt(JOptionPane.showInputDialog("What minute is it"));
+        int secs = Integer.parseInt(JOptionPane.showInputDialog("What second is it"));
+        Clock BedroomClock = new Clock(hours,mins,secs);
         System.out.println(BedroomClock);
     }
 }
--- class design/StudentDriver.java
+++ class design/StudentDriver.java
@@ -1,9 +1,13 @@
+import javax.swing.JOptionPane;
 public class StudentDriver{
     public static void main(String[] args){
+  
         Student senior001 = new Student();
         System.out.println(senior001);
         Student junior001 = new Student("ted", "Gibbson", 16 , 0.8);
         System.out.println(junior001);
-        JOptionPane.showMessageDialog(senior001);
+        JOptionPane.showMessageDialog(null, senior001);
+        JOptionPane.showMessageDialog(null, junior001);
     }
 }
+
