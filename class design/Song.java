8552bb9d2e474df9d7f35160b42e351cb1de2262
Peter <peter2524@cmis.ac.th>
2018-02-13 09:51:28 +0700

    did work on the class method problems assingment

--- class design/Song.java
+++ class design/Song.java
@@ -1,9 +1,10 @@
 public class Song{
-    private double length;
+    private int length;
     private int tempo;
     private String name = "";
+    private int yearReleased;
 
-    public Song(String name, int tempo, double length){
+    public Song(String name, int tempo, int length, int yearReleased){
         if(name == ""){
             this.name = "Untitled";
         }else{
@@ -14,24 +15,46 @@ public class Song{
         }else{
             this.tempo = 100;
         }
-        if(length>0.0){
+        if(length>0){
             this.length = length;
         }else{
-            this.length = 3.30;
+            this.length = 330;
+        }if(yearReleased<=0){
+            this.yearReleased = 1998;
+        }else{
+            this.yearReleased = yearReleased;
         }
     }
-    
+
     public Song(){
         this.name = "Untitled";
-        this.length = 0.0;
+        this.length = 000;
         this.tempo = 0;
+        this.yearReleased = 1998;
     }
-    
+
+    public int getYearReleased(){
+        return this.yearReleased;
+    }
+
+    public void setYearReleased(int newYear){
+        this.yearReleased = newYear;
+    }
+
+    public String convertToMinSec(){
+        String out = "";
+        int secs = this.length%60;
+        int mins = (length-secs)/60;
+        out = mins+"mins and "+secs+" seconds.";
+        return out;
+    }
+
     public String toString(){
         String out = "";
         out+= "Song name: " + name + "\n";
-        out+= "Song duration(mins.secs): " + length + "\n";
+        out+= "Song duration: "+ convertToMinSec() + "\n";
         out+= "Tempo: " + tempo +"\n";
+        out+= "Year Released: "+ yearReleased + "\n";
         return out;
     }
 }
--- class design/SongDriver.java
+++ class design/SongDriver.java
@@ -1,8 +1,13 @@
+import javax.swing.JOptionPane; 
 public class SongDriver{
     public static void main(String[] args){
         Song song1 = new Song();
         System.out.println(song1);
-        Song song2 = new Song("Hatred Copter", 110, 2.56);
+        Song song2 = new Song("Hatred Copter", 110, 176, 2006);
         System.out.println(song2);
+        String newYear = JOptionPane.showInputDialog("what year was the song released in");
+        int result = Integer.parseInt(newYear);
+        song1.setYearReleased(result);
+        JOptionPane.showMessageDialog(null,"the year is"+song1.getYearReleased());
     }
 }
