ffc3edf0423535e667468eccf38e84af8b30bd89
peter <peter2524@cmis.ac.th>
2018-02-15 14:32:12 +0700

    im up to question 5

--- class design/Student.java
+++ class design/Student.java
@@ -5,6 +5,7 @@ public class Student{
     private int age = 18;
     private double height = 1.90;// metres 
     private double[] grades = new double[5];
+    private String[] letters = new String[5];
     public Student(){
         this.fname = fname;
         this.lname = lname;
@@ -28,17 +29,71 @@ public class Student{
         this.grades[1] = math;
         this.grades[2] = sci;
         this.grades[3] = fArts;
-        this.grades[4] = socsci
+        this.grades[4] = socsci;
         this.lname = lname;
         this.age = age;
         this.height = height;
     }
+
+    public void setGPA(double eng, double math, double sci,
+    double fArts, double socSci){
+        this.grades[0] = eng;
+        this.grades[1] = math;
+        this.grades[2] = sci;
+        this.grades[3] = fArts;
+        this.grades[4] = socSci;
+    }
+
+    public void letterSetter(){
+        for( int x = 0; x< grades.length; x++){
+            if(grades[x]>=4.0){
+                letters[x] = "A";
+            }else if(grades[x]>=3.7){
+                letters[x] = "A-";
+            }else if(grades[x]>=3.3){
+                letters[x] = "B+";
+            }else if(grades[x]>=3.0){
+                letters[x] = "B";
+            }else if(grades[x]>=2.7){
+                letters[x] = "B-";
+            }else if(grades[x]>=2.3){
+                letters[x] = "C+";
+            }else if(grades[x]>=2.0){
+                letters[x] = "C";
+            }else if(grades[x]>=1.7){
+                letters[x] = "C-";
+            }else if(grades[x]>=1.3){
+                letters[x] = "D+";
+            }else if(grades[x]>=1.0){
+                letters[x] = "D";
+            }else if(grades[x]>=.7){
+                letters[x] = "D-";
+            }else if(grades[x]>=.3){
+                letters[x] = "f";
+            }else{
+                letters[x] = " dude how dumb are you";
+            }
+        }
+    }
+
+    public double calcGPA(){
+        double gpa = (grades[0]+grades[1]+grades[2]+grades[3]+grades[4])/5;
+        return gpa;
+    }
+
     public String toString(){
+        letterSetter();
         String out = "";
         out += "First Name: " +fname+"\n";
         out += "Last Name: " +lname+"\n";
         out += "Age: " +age+"\n";
         out += "Height: " +height+"\n";
+        out += "Grade in English: " +grades[0]+ letters[0]+"\n";
+        out += "Grade in Math: " +grades[1]+letters[1]+"\n";
+        out += "Grade in Science: " +grades[2]+letters[2]+"\n";
+        out += "Grade in Fine Arts: " +grades[3]+letters[3]+"\n";
+        out += "Grade in Social Science: " +grades[4]+letters[4]+"\n";
+        out += "GPA: "+ calcGPA();
         return out;
     }
 }
--- class design/StudentDriver.java
+++ class design/StudentDriver.java
@@ -1,13 +1,22 @@
 import javax.swing.JOptionPane;
 public class StudentDriver{
     public static void main(String[] args){
-  
+
         Student senior001 = new Student();
         System.out.println(senior001);
-        Student junior001 = new Student("ted", "Gibbson", 16 , 0.8);
+        Student junior001 = new Student("ted", "Gibbson", 16 , 0.8 , 4.0, 4.0, 4.0, 4.0, 4.0);
         System.out.println(junior001);
         JOptionPane.showMessageDialog(null, senior001);
-        JOptionPane.showMessageDialog(null, junior001);
+        JOptionPane.showMessageDialog(null, junior001);        
+        Student joe = new Student();
+        System.out.println(joe);
+        double eng = Double.parseDouble(JOptionPane.showInputDialog("what is his english grade"));
+        double math = Double.parseDouble(JOptionPane.showInputDialog("what is his math grade"));
+        double sci = Double.parseDouble(JOptionPane.showInputDialog("what is his science grade"));
+        double fArts = Double.parseDouble(JOptionPane.showInputDialog("what is his Fine arts grade"));
+        double socSci = Double.parseDouble(JOptionPane.showInputDialog("what is his social science grade"));
+        joe.setGPA(eng,math,sci,fArts,socSci);
+        System.out.println(joe);
     }
 }
 
