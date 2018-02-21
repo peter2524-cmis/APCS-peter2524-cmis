df9a0bcf2253f4a36dded701c798bdd60a07b6f3
peternicholson <peter2524@cmis.ac.th>
2018-02-21 13:30:42 +0700

    asdjkfh

---
+++ class design/Compare.java
@@ -0,0 +1,12 @@
+
+public class Compare{
+    public static void main(String[] args){
+        String school1 = "Stanford";
+        String school2 = "Oregon";
+        String school3 = "SUCLA";
+        System.out.println( school3.compareTo(school1));
+
+    }
+    // 1:it tells us wich of these two words would apear first in the dictionary
+    // 2:it goes by the second letter
+}
---
+++ class design/CreateNewPassword.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class CreateNewPassword{
+    public static void main(String[] args){
+String password = JOptionPane.showInputDialog("give me a new pasword");
+    }
+    
+    public String generator(String pass){
+        
+        if((pass.length>6)&&(weirdtest == true)){
+            return pass;
+        }
+    }
+}
