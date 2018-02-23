81051b70a091ebf2f3d9853f44490e5ff424d4a9
peternicholson <peter2524@cmis.ac.th>
2018-02-23 10:25:35 +0700

    asdkjhg

---
+++ Class Compositions/MyPod.java
@@ -0,0 +1,10 @@
+public class MyPod
+{
+   private String colour;
+   private double stgb;
+   private String[] songs = new String[3];
+   public Mypod(){
+       this.colour = "red";
+       
+   }
+}
--- class design/CreateNewPassword.java
+++ class design/CreateNewPassword.java
@@ -1,13 +1,22 @@
 import javax.swing.JOptionPane;
 public class CreateNewPassword{
     public static void main(String[] args){
-String password = JOptionPane.showInputDialog("give me a new pasword");
+        String password = JOptionPane.showInputDialog("give me a new pasword");
     }
-    
+
     public String generator(String pass){
-        
-        if((pass.length>6)&&(weirdtest == true)){
+        boolean weirdtest = false;
+        for(int x = 0; x<pass.length();x++){
+            if((pass.substring(x,(x+1)) == "+") || (pass.substring(x,(x+1)) == "-") || (pass.substring(x,(x+1)) == "*") || (pass.substring(x,(x+1)) == "/")){
+                weirdtest = true;
+            }
+        }
+
+        if((pass.length()>6)&&(weirdtest == true)){
             return pass;
+        }else{
+            String password = JOptionPane.showInputDialog("give me a new pasword");
+            return generator(password);
         }
     }
 }
