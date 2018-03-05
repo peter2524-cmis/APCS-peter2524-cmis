e4bd6cc870f595520af4290cd89a3cb917bef4b7
Peter <peter2524@cmis.ac.th>
2018-03-05 11:55:23 +0700

    did work on number 3

--- Class Compositions/Roster.java
+++ Class Compositions/Roster.java
@@ -1,33 +1,29 @@
+public class Roster{
+    private Student[] roster = new Student[3];
+    public String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
 
-/**
- * Write a description of class Roster here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Roster
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Roster
-     */
-    public Roster()
-    {
-        // initialise instance variables
-        x = 0;
+    }
+    public Roster(){
+        for(int x = 0; x< roster.length; x++){
+            roster[x] = new Student();
+        }
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public Roster(String x){
+        for(int x = 0; x<roster.length; x++){
+            String fname = getSaltString();
+            String lname = getSaltString();
+            Student(fname,lname, int age, double height, double eng,
+            double math , double sci, double fArts, double socsci )
+        }
     }
 }
