66ad311532ed5c7233baf37dffe2abbffaa1952c
Peter <peter2524@cmis.ac.th>
2018-03-06 13:57:52 +0700

    did all of the stuff for question 3 and started looking at question 4

--- Class Compositions/Roster.java
+++ Class Compositions/Roster.java
@@ -1,7 +1,9 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 public class Roster{
     private Student[] roster = new Student[3];
     public String getSaltString() {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
         while (salt.length() < 18) { // length of the random string.
@@ -12,6 +14,7 @@ public class Roster{
         return saltStr;
 
     }
+
     public Roster(){
         for(int x = 0; x< roster.length; x++){
             roster[x] = new Student();
@@ -19,11 +22,33 @@ public class Roster{
     }
 
     public Roster(String x){
-        for(int x = 0; x<roster.length; x++){
+        for(int i = 0; i<roster.length; i++){
             String fname = getSaltString();
             String lname = getSaltString();
-            Student(fname,lname, int age, double height, double eng,
-            double math , double sci, double fArts, double socsci )
+            int age = 6 + (int)(Math.random() * ((18 - 6) + 1));
+            double height = 1.0 + (double)(Math.random() * ((1.8 - 1) + 1.0));
+            double eng = 0.0 + (double)(Math.random() * ((4.0 - 0) + 0));
+            double math = 0.0 + (double)(Math.random() * ((4.0 - 0) + 0));
+            double sci = 0.0 + (double)(Math.random() * ((4.0 - 0) + 0));
+            double fArts = 0.0 + (double)(Math.random() * ((4.0 - 0) + 0));
+            double socsci = 0.0 + (double)(Math.random() * ((4.0 - 0) + 0));
+            roster[i] = new Student(fname,lname,age,height,eng,math ,sci,fArts,socsci );
         }
+    }
+
+    public Student findStudentWithMaxGPA(){
+        Student mrSmartyPants = new Student();
+        for(int i = 0; i<roster.length;i++){
+            if(roster[i].calcGPA()>mrSmartyPants.calcGPA()){
+                mrSmartyPants = roster[i];
+            }
+        }
+        return mrSmartyPants;
+    }
+    
+    public String toString(){
+        String out ="";
+        out+=findStudentWithMaxGPA();
+        return out;
     }
 }
---
+++ Class Compositions/RosterDriver.java
@@ -0,0 +1,7 @@
+public class RosterDriver{
+    public static void main(String[] args){
+        Roster class1 = new Roster("x");
+        System.out.println(class1);
+    }
+    
+}
